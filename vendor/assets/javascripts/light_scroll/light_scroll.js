c0de4daac6b44a4ad9fc0c0810c7029a516a68c3
dileep nandanam <dileepsnandanam@gmail.com>
2020-11-12 13:08:24 +0530

    Update light_scroll.js
    fixing page loads due network latency

--- vendor/assets/javascripts/light_scroll/light_scroll.js
+++ vendor/assets/javascripts/light_scroll/light_scroll.js
@@ -1,11 +1,17 @@
+
 initLightScroll = function() {
+  scroll_fired = null
   window.onscroll = function() {
     if($(window).height() + 600 + document.documentElement.scrollTop > $('body').height()) {
-      Rails.fire($('.view-more')[0], 'click')
+      if(scroll_fired == null) {
+        Rails.fire($('.view-more')[0], 'click')
+        scroll_fired = 1
+      }
     }
   }
   $(document).on('ajax:success', '.view-more', function(e) {
     $(this).closest('.more-items').replaceWith(e.detail[2].responseText)
+    scroll_fired = null
   })
 }
 $(document).on('turbolinks:load', function() {
@@ -14,3 +20,4 @@ $(document).on('turbolinks:load', function() {
 $(document).ready(function(){
   initLightScroll()
 })
+
